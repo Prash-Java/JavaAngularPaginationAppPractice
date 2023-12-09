9d538c9f190158772b0e67f8fab1683c8e0e2e92
prash <prash.sastra1@gmail.com>
2023-12-09 23:09:15 +0530

    Backend development updates

--- AngularAppPagination/src/main/java/com/java/pagination/angularapp/resource/UserResource.java
+++ AngularAppPagination/src/main/java/com/java/pagination/angularapp/resource/UserResource.java
@@ -4,13 +4,11 @@ import com.java.pagination.angularapp.domain.HttpResponse;
 import com.java.pagination.angularapp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.OK;
@@ -18,13 +16,15 @@ import static org.springframework.http.HttpStatus.OK;
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserResource {
     private final UserService userService;
 
     @GetMapping("/users")
     public ResponseEntity<HttpResponse> getUsers(@RequestParam Optional<String> name,
                                                  @RequestParam Optional<Integer> page,
-                                                 @RequestParam Optional<Integer> size) {
+                                                 @RequestParam Optional<Integer> size) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(3);
         return ResponseEntity.ok().body(
                 HttpResponse.builder()
                         .timeStamp(now().toString())
